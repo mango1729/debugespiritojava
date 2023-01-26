1c10c5b3d74fd7dcfaedb0d77177d49b3e0c8c88
manoj1729 <108476842+mango1729@users.noreply.github.com>
2023-01-26 21:27:36 +0530

    Create debug1.java

---
+++ debug1.java
@@ -0,0 +1,12 @@
+public class MAIN {
+
+  public static void MAIN(string[] args) {
+    scanner sc = new scanner(System.in)
+	rows = sc.nextInt();
+    for (int i = 0; i < rows; --i) {
+      for (int j = 0; j < 1; ++j) {
+        System.out.println(j + " ")
+        System.out.println()
+    }
+  }
+}
