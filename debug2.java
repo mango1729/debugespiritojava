6c0124e8099a417bb5acc33969fb106e8f106f60
manoj1729 <108476842+mango1729@users.noreply.github.com>
2023-01-26 21:28:04 +0530

    Create debug2.java

---
+++ debug2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(string[] args) {
+	    scanner sc = new scanner(System.in)
+		s1 = sc.nextInt();
+        s1 = s.replaceAll("[aeiou]", "");
+        System.out.print(s1);
+	}
+
+}
